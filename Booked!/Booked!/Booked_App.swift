c422ba4a9bef364b4274e455f44ef39123638414
Gwen Bradshaw <gebradshaw@qu.edu>
2026-01-29 19:37:33 -0500

    add first codex files

---
+++ Booked!/Booked!/Booked_App.swift
@@ -0,0 +1,17 @@
+//
+//  Booked_App.swift
+//  Booked!
+//
+//  Created by gwen bradshaw on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Booked_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
